dfed4af129a207b47d14c2633d1ad2a19211e1f7
AndrewOlsen <andrewolsen@mail.adelphi.edu>
2018-01-09 14:17:25 -0500

    coin.sh

---
+++ .scripts/coins.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+#
+# Use coinmarketcap.com API to retrieve coin values in USD
+
+echo "BTC: $(curl https://api.coinmarketcap.com/v1/ticker/bitcoin/ -s | jq '.[].price_usd' -r)"
+echo "ETH: $(curl https://api.coinmarketcap.com/v1/ticker/ethereum/ -s | jq '.[].price_usd' -r)"
+echo "LTE: $(curl https://api.coinmarketcap.com/v1/ticker/litecoin/ -s | jq '.[].price_usd' -r)"
+echo "DOGE: $(curl https://api.coinmarketcap.com/v1/ticker/dogecoin/ -s | jq '.[].price_usd' -r)"
